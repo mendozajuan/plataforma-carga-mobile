9e53facd8d8bb9a2b6b835231ebce5978de3af1a
Juan Mendoza <jmendoza@despegar.com>
2015-12-20 21:21:15 -0300

    Se agrega mapa de busqueda

--- app/src/main/java/com/movile/plataforma/plataformadecarga/user/LoginActivity.java
+++ app/src/main/java/com/movile/plataforma/plataformadecarga/user/LoginActivity.java
@@ -1,13 +1,14 @@
 package com.movile.plataforma.plataformadecarga.user;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.movile.plataforma.plataformadecarga.R;
+import com.movile.plataforma.plataformadecarga.user.consumer.search.UserConsumerSearch;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,7 +16,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
     }
 
+    public void toSearch(View view) {
+        Intent intent = new Intent(this, UserConsumerSearch.class);
+        startActivity(intent);
+    }
 }
